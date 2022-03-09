5ae339fd679e1a444c6861f518516541685a9598
RyanDeLong <rdelong111@gmail.com>
2022-03-09 13:54:00 -0600

    commit tree 2e191c5458e5add808d8f480aea20de4f00719b7

--- javascript/distinct_pair_sum.js
+++ javascript/distinct_pair_sum.js
@@ -1,5 +1,16 @@
 function distinctPairSum(arr, k) {
-  // type your code here
+  const ans = {};
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] + arr[i + 1] === k) {
+      if (!(`[${arr[i]}, ${arr[i + 1]}]` in ans)) {
+        ans[`[${arr[i]}, ${arr[i + 1]}]`] = [arr[i], arr[i + 1]];
+      }
+      i++;
+    }
+  }
+
+  return Object.values(ans);
 }
 
 if (require.main === module) {
@@ -15,5 +26,22 @@ if (require.main === module) {
 
 module.exports = distinctPairSum;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/*
+Initialize an empty object called ans
+
+Loop arr.length - 1 times
+  If current iteration + next iteration equals to k
+    If ordered pair is not in the object ans
+      Put the ordered pair in the object ans
+    Increment i
+
+Return the values of ans
+*/
+
+/*
+An empty object is initialized to store the correct ordered pairs and to quickly search if a current correct ordered pairs already exists.
+A for loop is created to iterate the length of arr - 1 times because the current iteration will always be compared to the next.
+The IF statement checks if the ordered pair sum is equal to k and increments i so the next iteration will be skipped. This is because the only possible outcomes of the next iteration will either be a correct ordered pair that already exists or an incorrect ordered pair.
+The nested IF statement checks if the ordered pair does not exists already. Then the ordered pair is stored in ans.
+When the FOR loop is completed, the the values of ans are returned in an array. 
+*/
